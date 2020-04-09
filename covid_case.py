ac315aa77007d3c843bf759da3f2b4cafa32747f
kenftw <53624574+kenftw@users.noreply.github.com>
2020-04-09 14:47:54 -0400

    script

---
+++ covid_case.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 23 15:11:50 2020
+
+@author: hewit
+"""
+import pandas as pd
+import matplotlib.pyplot as plt
+df = pd.read_csv('C:/Users/hewit/Documents/GitHub/COVID-19/archived_data/archived_time_series/time_series_19-covid-Confirmed_archived_0325.csv')
+
